f834ccbac493ac5c6be26dd52540f2e6d0b72ae4
Zester Quinn Albano <zester.albano@lanexus.com>
2022-09-24 17:46:39 +0800

    update player script

--- scripts/player.gd
+++ scripts/player.gd
@@ -16,7 +16,7 @@ var velocity := Vector2.ZERO
 var direction := Vector2.RIGHT
 
 var can_pickup := false
-var current_item_in_area = []
+var current_items_in_area = []
 
 
 func _ready() -> void:
@@ -25,7 +25,7 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	if(can_pickup and Input.is_action_just_pressed("doing")):
-		pickup_collectible()
+		pickup_collectible_item()
 
 
 func _physics_process(delta: float) -> void:
@@ -47,36 +47,48 @@ func set_sprite_direction() -> void:
 	if direction_key in DIRECTION_TO_FRAME:
 		animated_sprite.flip_h = sign(direction.x) == -1
 
-func pickup_collectible() -> void:
-	for item in current_item_in_area:
+
+func pickup_collectible_item() -> void:
+	for item in current_items_in_area:
+		var collectible_type
+
 		if(item.is_in_group("mushroom_orange")):
-			emit_signal("on_pickup_collectible", "mushroom_orange")
-			item.queue_free()
+			collectible_type = "mushroom_orange"
+		if(item.is_in_group("mushroom_brown")):
+			collectible_type = "mushroom_brown"
+		if(item.is_in_group("stone_yellow")):
+			collectible_type = "stone_yellow"
+		if(item.is_in_group("stone_red")):
+			collectible_type = "stone_red"
+
+		emit_signal("on_pickup_collectible", collectible_type)
+		item.queue_free()
 
 
 func show_dialog_box(text: String) -> void:
 	diaglog_box.text = text
 	diaglog_box.visible = true
 
+
 func hide_dialog_box() -> void:
 	diaglog_box.text = ""
 	diaglog_box.visible = false
 
+
 func _on_CollectArea_body_entered(body: Node) -> void:
 	if(body.is_in_group("collectibles")):
 		can_pickup = true
-		current_item_in_area.append(body)
+		current_items_in_area.append(body)
 		show_dialog_box("Press F to collect")
-
 
 
 func _on_CollectArea_body_exited(body: Node) -> void:
 	if(body.is_in_group("collectibles")):
 		can_pickup = false
-		var item_index = current_item_in_area.find(body)
-		current_item_in_area.remove(item_index)
+		var item_index = current_items_in_area.find(body)
+		current_items_in_area.remove(item_index)
 
-	if(current_item_in_area.size() == 0):
-		hide_dialog_box()
+		if(current_items_in_area.size() == 0):
+			hide_dialog_box()
 
 
