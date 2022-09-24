a2b52ee04de2fcd9978ad86d5059ec692b469297
Zester Quinn Albano <zester.albano@lanexus.com>
2022-09-24 16:38:31 +0800

    add ui

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,8 @@ extends KinematicBody2D
 onready var animated_sprite := $AnimatedSprite
 onready var diaglog_box := $DialogBox/Label
 
+signal on_pickup_collectible(type)
+
 const SPEED := 75
 const DIRECTION_TO_FRAME := {
 	Vector2.DOWN: 0,
@@ -13,10 +15,17 @@ const DIRECTION_TO_FRAME := {
 var velocity := Vector2.ZERO
 var direction := Vector2.RIGHT
 
+var can_pickup := false
+var current_item_in_area = []
+
 
 func _ready() -> void:
 	diaglog_box.visible = false
 	animated_sprite.flip_h = false if direction.x == 1 else true
+
+func _process(delta: float) -> void:
+	if(can_pickup and Input.is_action_just_pressed("doing")):
+		pickup_collectible()
 
 
 func _physics_process(delta: float) -> void:
@@ -27,6 +36,7 @@ func _physics_process(delta: float) -> void:
 		animated_sprite.play("run")
 	else:
 		animated_sprite.play("idle")
+
 	set_sprite_direction()
 
 
@@ -37,6 +47,13 @@ func set_sprite_direction() -> void:
 	if direction_key in DIRECTION_TO_FRAME:
 		animated_sprite.flip_h = sign(direction.x) == -1
 
+func pickup_collectible() -> void:
+	for item in current_item_in_area:
+		if(item.is_in_group("mushroom_orange")):
+			emit_signal("on_pickup_collectible", "mushroom_orange")
+			item.queue_free()
+
+
 func show_dialog_box(text: String) -> void:
 	diaglog_box.text = text
 	diaglog_box.visible = true
@@ -45,5 +62,21 @@ func hide_dialog_box() -> void:
 	diaglog_box.text = ""
 	diaglog_box.visible = false
 
+func _on_CollectArea_body_entered(body: Node) -> void:
+	if(body.is_in_group("collectibles")):
+		can_pickup = true
+		current_item_in_area.append(body)
+		show_dialog_box("Press F to collect")
+
+
+
+func _on_CollectArea_body_exited(body: Node) -> void:
+	if(body.is_in_group("collectibles")):
+		can_pickup = false
+		var item_index = current_item_in_area.find(body)
+		current_item_in_area.remove(item_index)
+
+	if(current_item_in_area.size() == 0):
+		hide_dialog_box()
 
 
---
+++ scripts/ui_layer.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+onready var mushroom_orange_count = $Collectibles/MushroomOrange/Count
+onready var mushroom_brown_count = $Collectibles/MushroomOrange/Count
+onready var stone_yellow_count = $Collectibles/MushroomOrange/Count
+onready var stone_red_count = $Collectibles/MushroomOrange/Count
+
+func _ready() -> void:
+	pass
+
+
+func _on_Player_on_pickup_collectible(type) -> void:
+	get(type + '_count').text = String(int(get(type + '_count').text) + 1)
